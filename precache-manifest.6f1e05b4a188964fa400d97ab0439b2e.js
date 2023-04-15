c3d11fb9ca4c305e2c56c0943d7adc37042b5890
Z3R0-NV5 <z3r0nv5@gmail.com>
2023-04-15 16:00:06 -0500

    Updates

---
+++ precache-manifest.6f1e05b4a188964fa400d97ab0439b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29e4eb284271680de3f0",
+    "url": "/static/css/main.ea24b0de.chunk.css"
+  },
+  {
+    "revision": "29e4eb284271680de3f0",
+    "url": "/static/js/main.29e4eb28.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "0bea6cf7ed968d86120d",
+    "url": "/static/js/2.0bea6cf7.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "14ca6f2c615e1117c124d3af68a54a45",
+    "url": "/index.html"
+  }
+];
