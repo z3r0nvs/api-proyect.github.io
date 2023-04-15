15760445281716dcb28487f8ce1c6aae51e7f5e5
Z3R0-NV5 <z3r0nv5@gmail.com>
2023-04-15 14:00:16 -0500

    New settings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
 import Table from './Table';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/Home' element={<Home />} />
-      </Routes>
+      
       <Routes>
         <Route path='/Table' element={<Table />} />
       </Routes>
